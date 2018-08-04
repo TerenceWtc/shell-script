b213781a8ff567a6fe04b1140eadad258018cfd7
terence <1464652235@qq.com>
2018-08-04 13:26:49 +0800

    make restart.sh better and add description into README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # shell-script
+
+write some linux scripts for the actions which were usually done
--- restart_jar/restart.sh
+++ restart_jar/restart.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# jdk
+# replace your JAVA_HOME.
 export JAVA_HOME=/home/centos/jdk/jdk1.8.0_181
-
-# path
 export PATH=$JAVA_HOME/bin:$PATH
 
+# replace your jar_file_name.
+FILENAME=cts-admin-console
+
 DATE=$(date +%Y-%m-%d:%H-%M-%S)
-#start java
-echo "start shell...."
+
+# start java...
+echo "starting shell..."
 APPDIR=`pwd`
 PIDFILE=$APPDIR/app.pid
+# if pid exsist, kill the process.
 if [ -f "$PIDFILE" ] && kill -0 $(cat "$PIDFILE"); then
     echo "already running..."
     echo "stopping..."
@@ -20,7 +23,9 @@ if [ -f "$PIDFILE" ] && kill -0 $(cat "$PIDFILE"); then
     echo "stopped!"
 fi
 echo "starting..."
-nohup java -jar $APPDIR/cts-admin-console.jar > ./logs/$DATE.log 2>&1 &
+# run at background and write the logs into file.
+nohup java -jar $APPDIR/$FILENAME.jar > ./logs/$FILENAME/$DATE.log 2>&1 &
+# write the process into file.
 echo $! > $PIDFILE
-echo "start success!"
+echo "starting success!"
 echo "waiting to startup..."
